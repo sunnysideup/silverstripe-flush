af6ba0d2502c4cb3810f240a118351ca108e9561
thiemen <nt@sunnysideup.co.nz>
2019-10-06 12:12:02 +1300

    PATCH: doing it

---
+++ src/FlushNow.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Sunnysideup\Flush;
+
+use SilverStripe\ORM\DB;
+use SilverStripe\Control\Director;
+
+trait FlushNow {
+
+
+    /**
+     * Show a message about task currently running
+     *
+     * @param string $message to display
+     * @param string $type one of [created|changed|repaired|obsolete|deleted|error]
+     * @param boolean $bullet add a bullet to message?
+     *
+     **/
+    public static function flushNow($message, $type = '', $bullet = true)
+    {
+        if(is_array($message)) {
+            $message = print_r($message);
+        }
+        echo '';
+        // check that buffer is actually set before flushing
+        if (ob_get_length()) {
+            @ob_flush();
+            @flush();
+            @ob_end_flush();
+        }
+        @ob_start();
+        if (Director::is_cli()) {
+            $message = strip_tags($message);
+        }
+        if ($bullet) {
+            DB::alteration_message($message, $type);
+        } else {
+            echo $message;
+        }
+    }
+
+
+    /**
+     * Show a message about task currently running
+     *
+     * @param string $message to display
+     * @param string $type one of [created|changed|repaired|obsolete|deleted|error]
+     * @param boolean $bullet add a bullet to message?
+     *
+     **/
+    protected function flushNowLine()
+    {
+        $this->flushNow('--------------------------------------------------------');
+    }
+
+}
