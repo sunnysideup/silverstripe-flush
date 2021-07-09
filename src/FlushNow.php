325564424dbca583c8f7ab1a238d696ffe34857f
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-07-10 09:25:40 +1200

    WIP

--- src/FlushNow.php
+++ src/FlushNow.php
@@ -21,9 +21,12 @@ trait FlushNow
         }
         @ob_start();
         if (Director::is_cli()) {
-            $message = strip_tags($message);
+            $colour = self::flush_now_type_to_colour($type, false);
+            $colour = $this->getColour($colour, false);
+            $outputString = "\033[" . $colour . strip_tags($message) . "\033[0m";
         } else {
-            $message = '<span style="color: ' . self::flush_now_type_to_colour($type) . '">' . $message . '</span>';
+            $colour = self::flush_now_type_to_colour($type, true);
+            $message = '<span style="color: ' .  $colour. '">' . $message . '</span>';
         }
         if ($bullet) {
             DB::alteration_message($message, $type);
@@ -74,12 +77,87 @@ trait FlushNow
             case 'repaired':
                 return 'blue';
             case 'deleted':
+            case 'error':
             case 'bad':
                 return 'red';
             case 'heading':
-                return 'orange';
+                return 'pink';
             default:
                 return 'black';
         }
     }
+
+
+
+    protected function getColour(string $colour, ?bool $usehtmlColour = false)
+    {
+        $htmlColour = str_replace('_', '', $colour);
+        $htmlColour = str_replace('-', '', $htmlColour);
+        switch ($colour) {
+            case 'black':
+                $colour = '0;30m';
+                break;
+            case 'blue':
+                $colour = '0;34m';
+                break;
+            case 'light_blue':
+                $colour = '1;34m';
+                break;
+            case 'green':
+                $colour = '0;32m';
+                break;
+            case 'light_green':
+                $colour = '1;32m';
+                break;
+            case 'cyan':
+                $colour = '0;36m';
+                break;
+            case 'light_cyan':
+                $colour = '1;36m';
+                break;
+            case 'red':
+            case 'error':
+                $colour = '0;31m';
+                $htmlColour = 'red';
+                break;
+            case 'light_red':
+                $colour = '1;31m';
+                $htmlColour = 'pink';
+                break;
+            case 'purple':
+                $colour = '0;35m';
+                break;
+            case 'run':
+            case 'light_purple':
+                $colour = '1;35m';
+                $htmlColour = 'violet';
+                break;
+            case 'brown':
+                $colour = '0;33m';
+                break;
+            case 'yellow':
+            case 'warning':
+                $colour = '1;33m';
+                $htmlColour = 'yellow';
+                break;
+            case 'light_gray':
+                $colour = '0;37m';
+                $htmlColour = '#999';
+                break;
+            case 'white':
+                $colour = '1;37m';
+                break;
+            case 'dark_gray':
+            case 'notice':
+            default:
+                $colour = '1;30m';
+                $htmlColour = '#555';
+        }
+        if ($usehtmlColour) {
+            return $htmlColour;
+        }
+        return $colour;
+    }
+
+
 }
