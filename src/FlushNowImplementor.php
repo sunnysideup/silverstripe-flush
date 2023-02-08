e4336dabe60ce77ca74360e826a74ac7e3d3ce4c
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-02-08 22:19:31 +1300

    WIP

---
+++ src/FlushNowImplementor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sunnysideup\Flush;
+
+use Sunnysideup\Flush\FlushNow;
+
+use SilverStripe\Control\Director;
+use SilverStripe\ORM\DB;
+
+class FlushNowImplementor
+{
+    use FlushNow;
+}
